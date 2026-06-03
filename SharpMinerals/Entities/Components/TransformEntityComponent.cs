888e6b13c6dbe69fb1caa46a3d652f3c45a63951
__tacoguy <apotroend@outlook.com>
2026-06-03 20:15:45 +0200

    fix: Use Vector3m for TransformEntityComponent

--- SharpMinerals/Entities/Components/TransformEntityComponent.cs
+++ SharpMinerals/Entities/Components/TransformEntityComponent.cs
@@ -4,13 +4,19 @@ namespace SharpMinerals.Entities.Components;
 /// visible entity carries.</summary>
 [Component]
 public struct TransformEntityComponent {
-    public Mfloat X, Y, Z;
+    public Vector3m Position;
+    public Mfloat X { get => Position.X; set => Position.X = value; }
+    public Mfloat Y { get => Position.Y; set => Position.Y = value; }
+    public Mfloat Z { get => Position.Z; set => Position.Z = value; }
     public float Yaw, Pitch;
 
-    public TransformEntityComponent(double x, double y, double z, float yaw = 0f, float pitch = 0f) {
-        X = x; Y = y; Z = z;
+    public TransformEntityComponent(Vector3m position, float yaw = 0f, float pitch = 0f) {
+        Position = position;
         Yaw = yaw; Pitch = pitch;
     }
-
-    public readonly Vector3m Position => new(X, Y, Z);
+    public TransformEntityComponent(Mfloat x, Mfloat y, Mfloat z, float yaw = 0f, float pitch = 0f) {
+        Position = new(x, y, z);
+        Yaw = yaw;
+        Pitch = pitch;
+    }
 }
